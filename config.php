c4aebae3a9273c28d256ab1ad0bc5a1edd233aae
ImDong <www@qs5.org>
2017-05-06 15:11:09 +0800

    无

--- config.php
+++ config.php
@@ -1,1 +1,23 @@
 <?php
+/**
+ * QsMail POP3 Server
+ * 基于Swoole的POP3邮件服务器
+ *
+ * @author      青石 <www@qs5.org>
+ * @copyright   QsMail POP3 Server
+ */
+
+/**
+ * 配置信息文件
+ */
+return array(
+
+    //**** 数据库信息配置 ****//
+    'DB_CONFIG' => array(
+        'host'     => 'localhost',
+        'username' => 'mail',
+        'password' => '123456',
+        'db'       => 'qsmail',
+        'charset'  => 'utf8'
+    ),
+);
